9ea7bfdbd5d863b2a684ca749d481b41be079de5
Edwidge Amisial <edwidge.amisial@gmail.com>
2024-01-09 22:08:54 -0500

    Add project files.

---
+++ BlazorEcommerce/Server/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+  private readonly ApplicationDbContext _context;
+
+  public ProductsController(ApplicationDbContext context)
+  {
+    _context = context;
+  }
+
+  [HttpGet]
+  public async Task<ActionResult<List<Product>>> GetProducts()
+  {
+    var products = await _context.Products.ToListAsync();
+    return Ok(products);
+  }
+}
---
+++ BlazorEcommerce/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorEcommerce.Server.Data;
+
+public class ApplicationDbContext : DbContext
+{
+  public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+  public DbSet<Product> Products { get; set; }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    modelBuilder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+  }
+}
